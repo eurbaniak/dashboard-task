a5aec6e3de7cf00bcd72f78152af310707e30acc
Edwin <50717284+Adderall-dev@users.noreply.github.com>
2023-09-30 14:56:52 +0200

    remove unused code, add mantine, jest, redux and mock data

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "@mantine/core/styles.css";
+import { MantineProvider, createTheme } from "@mantine/core";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const theme = createTheme({
+  primaryColor: "cyan",
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <MantineProvider theme={theme} defaultColorScheme="dark">
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
+);
